7121888a330ece23bbbf010d857e11f0e8f57910
Tim Payne <tim.tpayne@gmail.com>
2020-12-18 16:37:45 +0000

    Update doc

--- get-started.sh
+++ get-started.sh
@@ -5,17 +5,3 @@ brew tap hashicorp/tap
 brew install hashicorp/tap/terraform
 brew upgrade hashicorp/tap/terraform
 terraform -install-autocomplete
-
-# Initialise project...
-cd samples
-cd docker
-
-terraform init
-
-# Create your .tf files as needed
-
-# To dry run deployments...
-#terraform plan
-
-# To apply deployments...
-#terraform apply or terraform apply -auto-approve
