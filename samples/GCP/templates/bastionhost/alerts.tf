5ad744d4e9f52fd9967727583aa4d2a64a4dac99
Tim Payne <tim.tpayne@gmail.com>
2021-06-05 23:52:04 +0100

    Add SLOs

--- samples/GCP/templates/bastionhost/alerts.tf
+++ samples/GCP/templates/bastionhost/alerts.tf
@@ -26,6 +26,9 @@
 # This section will declare the providers needed...
 # terraform init -upgrade
 
+//
+// Various sample alerts
+//
 resource "google_monitoring_alert_policy" "lb_alert001" {
   // L7 load balancer error request count = 0/sec
   display_name = "Availability HTTP/S Load Balancing Rule"
@@ -174,6 +177,132 @@ resource "google_monitoring_alert_policy" "cpu_alert001" {
   }
   documentation {
     content = "The CPU rule $${condition.display_name} has generated this alert for the $${metric.display_name}."
+  }
+}
+
+//
+// Various sample SLOs
+//
+resource "google_monitoring_custom_service" "service_slo" {
+  service_id   = "service-slo"
+  display_name = "Service SLO"
+}
+
+resource "google_monitoring_slo" "slo_lbbackend" {
+  service      = google_monitoring_custom_service.service_slo.service_id
+  display_name = "Standard SLO LB Backend latency"
+
+  goal                = 0.8
+  rolling_period_days = 20
+
+  windows_based_sli {
+    window_period = "100s"
+
+    good_total_ratio_threshold {
+      threshold = 0.1
+      performance {
+        distribution_cut {
+          distribution_filter = join(" AND ", [
+            "metric.type=\"loadbalancing.googleapis.com/https/backend_latencies\"",
+            "resource.type=\"https_lb_rule\"",
+          ])
+
+          range {
+            min = 1
+            max = 9
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "google_monitoring_slo" "slo_lbtlbackend" {
+  service      = google_monitoring_custom_service.service_slo.service_id
+  display_name = "Standard SLO LB Total latency"
+
+  goal                = 0.8
+  rolling_period_days = 20
+
+  windows_based_sli {
+    window_period = "100s"
+
+    good_total_ratio_threshold {
+      threshold = 0.1
+      performance {
+        distribution_cut {
+          distribution_filter = join(" AND ", [
+            "metric.type=\"loadbalancing.googleapis.com/https/total_latencies\"",
+            "resource.type=\"https_lb_rule\"",
+          ])
+
+          range {
+            min = 1
+            max = 9
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "google_monitoring_slo" "slo_monitor" {
+  service      = google_monitoring_custom_service.service_slo.service_id
+  display_name = "Standard SLO to monitor the monitor"
+
+  goal            = 0.95
+  calendar_period = "FORTNIGHT"
+
+  windows_based_sli {
+    window_period = "400s"
+    good_bad_metric_filter = join(" AND ", [
+      "metric.type=\"monitoring.googleapis.com/uptime_check/check_passed\"",
+      "resource.type=\"uptime_url\"",
+    ])
+  }
+}
+
+resource "google_monitoring_slo" "slo_gce" {
+  service      = google_monitoring_custom_service.service_slo.service_id
+  display_name = "Standard SLO to monitor the GCEs"
+
+  goal                = 0.9
+  rolling_period_days = 20
+
+  windows_based_sli {
+    window_period = "600s"
+    metric_mean_in_range {
+      time_series = join(" AND ", [
+        "metric.type=\"agent.googleapis.com/cassandra/client_request/latency/95p\"",
+        "resource.type=\"gce_instance\"",
+      ])
+
+      range {
+        max = 5
+      }
+    }
+  }
+}
+
+resource "google_monitoring_slo" "slo_monitor_up" {
+  service      = google_monitoring_custom_service.service_slo.service_id
+  display_name = "Standard SLO to monitor the monitor uptime"
+
+  goal                = 0.9
+  rolling_period_days = 20
+
+  windows_based_sli {
+    window_period = "400s"
+    metric_sum_in_range {
+      time_series = join(" AND ", [
+        "metric.type=\"monitoring.googleapis.com/uptime_check/request_latency\"",
+        "resource.type=\"uptime_url\"",
+      ])
+
+      range {
+        max = 5000
+      }
+    }
   }
 }
 
