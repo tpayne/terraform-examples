09b692d079a2611ac921ae7edf94f7301b72cc3a
Tim Payne <tpayne@users.noreply.github.com>
2021-09-17 15:57:38 +0100

    Create services.sh
    Small helper app for services

---
+++ samples/GCP/services.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+enable()
+{
+echo "Enable..."
+gcloud services enable compute.googleapis.com
+gcloud services enable cloudresourcemanager.googleapis.com
+gcloud services enable servicenetworking.googleapis.com
+gcloud services enable iam.googleapis.com sqladmin.googleapis.com
+gcloud services list
+}
+
+disable()
+{
+echo "Disable..."
+gcloud services disable compute.googleapis.com
+gcloud services disable cloudresourcemanager.googleapis.com
+gcloud services disable servicenetworking.googleapis.com
+gcloud services disable iam.googleapis.com sqladmin.googleapis.com
+gcloud services list
+}
+
+if [ "x$1" = "xenable" ]; then
+	enable
+else
+	disable
+fi
+
