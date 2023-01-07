9129772d7ec644a94537cd7d92a371ce79cd3720
Tim Payne <tpayne@users.noreply.github.com>
2023-01-07 22:58:30 +0000

    Fix script dir

--- docker-files/scripts/checker.sh
+++ docker-files/scripts/checker.sh
@@ -42,6 +42,8 @@ runTFLint() {
 runArmScan() {
   paths=$(getPaths "$@")
 
+  cd arm-ttk/
+
   for path in ${paths}; do
 pwsh << EOF
       Get-ChildItem *.ps1, *.psd1, *.ps1xml, *.psm1 -Recurse | Unblock-File
