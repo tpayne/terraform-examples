d976c807c9927d7321d1d836dddbf19eb82ebcc3
tpayne <tim.tpayne@gmail.com>
2024-02-16 11:06:52 +0000

    Use optimised example for mssql

--- samples/Azure/loose/modules/validateResource/modules/mssql/main.tf
+++ samples/Azure/loose/modules/validateResource/modules/mssql/main.tf
@@ -1,30 +1,30 @@
+locals {
+  toCheck = alltrue([
+              data.azurerm_mssql_database.toCheck.name == "server-db" &&
+              data.azurerm_mssql_database.toCheck.collation == "SQL_Latin1_General_CP1_CI_AS"
+            ])
+}
+
+data "azurerm_mssql_database" "toCheck" {
+  name      = var.resourceObj.name
+  server_id = var.resourceObj.server_id
+}
+
 // Check test
 check "resource_check" {
-  data "azurerm_mssql_database" "tocheck" {
-    name      = var.resourceObj.name
-    server_id = var.resourceObj.server_id
-  }
-
   assert {
     condition = alltrue([
-      data.azurerm_mssql_database.tocheck.name == "server-db" &&
-      data.azurerm_mssql_database.tocheck.collation == "SQL_Latin1_General_CP1_CI_AS"
+      local.toCheck
     ])
     error_message = "MSSQL failed validation"
   }
 }
 
 // To error
-data "azurerm_mssql_database" "toFail" {
-  name      = var.resourceObj.name
-  server_id = var.resourceObj.server_id
-}
-
-resource "null_resource" "asserttest" {
+resource "null_resource" "assertError" {
   count = (var.assertError) ? 1 : 0
   triggers = alltrue([
-    data.azurerm_mssql_database.toFail.name == "server-db" &&
-    data.azurerm_mssql_database.toFail.collation == "SQL_Latin1_General_CP1_CI_AS"
+    local.toCheck
   ]) ? {} : file("MSSQL failed validation")
 
   lifecycle {
